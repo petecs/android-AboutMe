9fbac366898a1ad8b0a037f6e4c0c6084d82a89d
Peter Akala <40690295+petecs@users.noreply.github.com>
2019-08-20 05:56:41 -0700

    created data class and bound data to data class object instead of using string resources

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -18,14 +18,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("Peter Akala")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.myName = myName
+
         //findViewById<Button>(R.id.done_button).setOnClickListener {
         //    addNickname(it)
         //}
+
         binding.doneButton.setOnClickListener {
             addNickname(it)
         }
@@ -34,7 +39,8 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(view: View) {
 
         binding.apply {
-            nicknameText.text = binding.nicknameEdit.text.toString()
+            //nicknameText.text = binding.nicknameEdit.text.toString()
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
